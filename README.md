f7ac16832bc4a68eb4e72008408ff02d10afa19b
quantumjot <code@arlowe.co.uk>
2020-09-09 11:50:01 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# UCL BIOC0003 Introduction to Python
+
+
+[![Binder](https://mybinder.org/badge_logo.svg)](https://mybinder.org/v2/gh/quantumjot/BIOC0003-IntroductiontoPython/master?filepath=index.ipynb)
+
+Repo for the class
---
+++ resources/autostyle.js
@@ -0,0 +1,13 @@
+// AUTORUN ALL CELLS ON NOTEBOOK-LOAD!
+require(
+    ['base/js/namespace', 'jquery'],
+    function(jupyter, $) {
+        $(jupyter.events).on("kernel_ready.Kernel", function () {
+            console.log("Auto-running all cells-below...");
+            // jupyter.actions.call('jupyter-notebook:run-all-cells-below');
+            // jupyter.actions.call('jupyter-notebook:save-notebook');
+
+            $("head").append("<link rel='stylesheet' id='extracss' href='/resources/style.css' type='text/css' />");
+        }
+    )}
+);
