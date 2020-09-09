4358e3a590cf56129635754c810af843b013b351
quantumjot <code@arlowe.co.uk>
2020-09-09 12:42:00 +0100

    updated autostyler

--- resources/autostyle.js
+++ resources/autostyle.js
@@ -1,15 +1,19 @@
 // AUTORUN ALL CELLS ON NOTEBOOK-LOAD!
+
+var cssPath = "./resources/style.css";
+var cssLink = "<link rel='stylesheet' id='extracss' href='"+cssPath+"' type='text/css' />";
+
 require(
     ['base/js/namespace', 'jquery'],
     function(jupyter, $) {
         $(jupyter.events).on("kernel_ready.Kernel", function () {
-            console.log("Auto-running all cells-below...");
+            // console.log("Auto-running all cells-below...");
             // jupyter.actions.call('jupyter-notebook:run-all-cells-below');
             // jupyter.actions.call('jupyter-notebook:save-notebook');
-
-            $("head").append("<link rel='stylesheet' id='extracss' href='./resources/style.css' type='text/css' />");
+            $("head").append(cssLink);
         }
     )}
 );
 
-$("head").append("<link rel='stylesheet' id='extracss' href='./resources/style.css' type='text/css' />");
+// re-run this if necessary
+$("head").append(cssLink);
